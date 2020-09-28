078e6340507e05a9cf8cc9793e5b045024eb69ca
rhdam2020 <71932022+rhdam2020@users.noreply.github.com>
2020-09-28 10:15:20 +0200

    mensaje
    -  mensaje personalizado

--- HolaMundo/HolaMundo/Program.cs
+++ HolaMundo/HolaMundo/Program.cs
@@ -9,7 +9,7 @@ namespace HolaMundo
             Console.WriteLine("\nCual es tu nombre? ");
             var nombre = Console.ReadLine();
             var fecha = DateTime.Now;
-            Console.WriteLine($"\nHola, {nombre}, a {fecha:d} at {fecha:t}!");
+            Console.WriteLine($"\nHola, {nombre}, a {fecha:d} a las {fecha:t}!");
             Console.Write("\nPresiona cualquier tecla para salir...");
             Console.ReadKey(true);
         }
